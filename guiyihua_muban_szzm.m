b43ed0615fdd46e09b8a8bf967e55f86ec63b1b6
Java <15591459159@163.com>
2020-09-26 15:49:57 +0800

    归一化处理2

---
+++ guiyihua_muban_szzm.m
@@ -0,0 +1,37 @@
+function shuzizimu = guiyihua_muban_szzm
+%0-9       ¹²10
+% ABCDE FGHJK LMNPQ RSTUV WXYZ   ¹²24
+shuzizimu(:,:,1)   =imresize(im2bw(imread('0.bmp'), graythresh(rgb2gray(imread('0.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,2)   =imresize(im2bw(imread('1.bmp'), graythresh(rgb2gray(imread('1.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,3)   =imresize(im2bw(imread('2.bmp'), graythresh(rgb2gray(imread('2.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,4)   =imresize(im2bw(imread('3.bmp'), graythresh(rgb2gray(imread('3.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,5)   =imresize(im2bw(imread('4.bmp'), graythresh(rgb2gray(imread('4.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,6)   =imresize(im2bw(imread('5.bmp'), graythresh(rgb2gray(imread('5.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,7)   =imresize(im2bw(imread('6.bmp'), graythresh(rgb2gray(imread('6.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,8)   =imresize(im2bw(imread('7.bmp'), graythresh(rgb2gray(imread('7.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,9)   =imresize(im2bw(imread('8.bmp'), graythresh(rgb2gray(imread('8.bmp')))), [110 70],'bilinear');
+shuzizimu(:,:,10)  =imresize(im2bw(imread('9.bmp'), graythresh(rgb2gray(imread('9.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,11)  =imresize(im2bw(imread('10.bmp'),graythresh(rgb2gray(imread('10.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,12)  =imresize(im2bw(imread('11.bmp'),graythresh(rgb2gray(imread('11.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,13)  =imresize(im2bw(imread('12.bmp'),graythresh(rgb2gray(imread('12.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,14)  =imresize(im2bw(imread('13.bmp'),graythresh(rgb2gray(imread('13.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,15)  =imresize(im2bw(imread('14.bmp'),graythresh(rgb2gray(imread('14.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,16)  =imresize(im2bw(imread('15.bmp'),graythresh(rgb2gray(imread('15.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,17)  =imresize(im2bw(imread('16.bmp'),graythresh(rgb2gray(imread('16.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,18)  =imresize(im2bw(imread('17.bmp'),graythresh(rgb2gray(imread('17.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,19)  =imresize(im2bw(imread('18.bmp'),graythresh(rgb2gray(imread('18.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,20)  =imresize(im2bw(imread('19.bmp'),graythresh(rgb2gray(imread('19.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,21)  =imresize(im2bw(imread('20.bmp'),graythresh(rgb2gray(imread('20.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,22)  =imresize(im2bw(imread('21.bmp'),graythresh(rgb2gray(imread('21.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,23)  =imresize(im2bw(imread('22.bmp'),graythresh(rgb2gray(imread('22.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,24)  =imresize(im2bw(imread('23.bmp'),graythresh(rgb2gray(imread('23.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,25)  =imresize(im2bw(imread('24.bmp'),graythresh(rgb2gray(imread('24.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,26)  =imresize(im2bw(imread('25.bmp'),graythresh(rgb2gray(imread('25.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,27)  =imresize(im2bw(imread('26.bmp'),graythresh(rgb2gray(imread('26.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,28)  =imresize(im2bw(imread('27.bmp'),graythresh(rgb2gray(imread('27.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,29)  =imresize(im2bw(imread('28.bmp'),graythresh(rgb2gray(imread('28.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,30)  =imresize(im2bw(imread('29.bmp'),graythresh(rgb2gray(imread('29.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,31)  =imresize(im2bw(imread('30.bmp'),graythresh(rgb2gray(imread('30.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,32)  =imresize(im2bw(imread('31.bmp'),graythresh(rgb2gray(imread('31.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,33)  =imresize(im2bw(imread('32.bmp'),graythresh(rgb2gray(imread('32.bmp')))),[110 70],'bilinear');
+shuzizimu(:,:,34)  =imresize(im2bw(imread('33.bmp'),graythresh(rgb2gray(imread('33.bmp')))),[110 70],'bilinear');
